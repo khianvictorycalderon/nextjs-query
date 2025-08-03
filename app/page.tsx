7b7d77ed88ae8166604b1e6d57afe3cdf28bcbfb
khian calderon <ckhianvictory@gmail.com>
2025-08-03 15:13:58 +0800

    Updated frontend (No functionality yet)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-semibold">Home Page</h1>
+        <p className="text-sm">Try adding a query at the url with ?name={"<name>"}</p>
+        <p className="text-sm">You can choose any name you want.</p>
+      </div>
+    </div>
   )
 }
