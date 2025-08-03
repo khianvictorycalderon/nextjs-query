a7b5174f0e2abda8e1b145e6d1b6d8d4f061a01b
khian calderon <ckhianvictory@gmail.com>
2025-08-03 15:16:35 +0800

    Added examples

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,14 @@ export default function Home() {
         <h1 className="text-4xl font-semibold">Home Page</h1>
         <p className="text-sm">Try adding a query at the url with ?name={"<name>"}</p>
         <p className="text-sm">You can choose any name you want.</p>
+        <p className="text-sm">Optional: You may add ?age parameter.</p>
+        <br/>
+        <p>Example:</p>
+        <ul className="list-disc list-inside">
+          <li>?name=John&age=18</li>
+          <li>?name=Jane&age=19</li>
+          <li>?name=Jake</li>
+        </ul>
       </div>
     </div>
   )
